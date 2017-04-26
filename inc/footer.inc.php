c5b8a5e3456b3e4bd67ea8991a91237938c45ef2
João Henrique <fontnelle@hotmail.com>
2017-04-26 18:01:39 -0300

    Update footer.inc.php
    incluindo Jquery.

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -6,7 +6,9 @@
 
 -->
 
-	<!-- Bootstrap -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<!-- jQuery (obrigatório para plugins JavaScript do Bootstrap) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
